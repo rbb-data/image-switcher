203947049a7897f343edd7130420139adb2540f4
Manuel Reich <mr@wundf.net>
2020-03-26 13:31:47 +0100

    Add simple edit link text

--- src/components/Editor/_EditLink.js
+++ src/components/Editor/_EditLink.js
@@ -5,6 +5,7 @@ const EditLink = () => {
   return (
     <>
       <h2>Edit Link</h2>
+      <p>copy this link to edit this component later</p>
       <CopyToClipboard code={window.location.href} copyText='copy edit link' />
     </>
   )
