0b64f1e60685cfffb4695eddf4e214d908b72fc4
heyarne <arne@schlueter.is>
2020-04-09 15:11:44 +0200

    First shot at making a slider

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,48 +2,23 @@
 
 import 'iframe-resizer/js/iframeResizer.contentWindow.js'
 
-import React, { useEffect, useState } from 'react'
-import qs from 'qs'
+import React from 'react'
 
 import _ from './App.module.sass'
-import ImageToggle from 'components/ImageToggle/ImageToggle'
-import Editor from 'components/Editor/Editor'
-
-// parse query string from location hash
-function parseQueryString () {
-  if (!window.location.hash.match(/\?/)) return {}
-  return qs.parse(window.location.hash.split('?')[1])
-}
-
-function useQueryString () {
-  const [query, setQuery] = useState(parseQueryString)
-  const onHashChange = _ => setQuery(parseQueryString())
-
-  useEffect(() => {
-    window.addEventListener('hashchange', onHashChange)
-    return function cleanup () {
-      window.removeEventListener('hashchange', onHashChange)
-    }
-  })
-
-  return query
-}
+import ImageSlider from 'components/ImageSlider/ImageSlider'
 
 function App (_props) {
-  const imageToggleConfig = useQueryString()
+  const sliderConfig = {
+    urlForValue: val => `./images/circle-${val}.png`,
+    min: 20,
+    max: 100,
+    step: 10,
+    label: 'Dauer der Maßnahme in Wochen:'
+  }
 
   return (
     <div className={_.app}>
-      {/* Appending &embed to the URL in any form will hide the following form
-        and create an embeddable version of this page. */}
-      {imageToggleConfig.embed == null ? (
-        <Editor images={imageToggleConfig.images} />
-      ) : (
-        <>
-          {/* show only the embed */}
-          <ImageToggle images={imageToggleConfig.images} />
-        </>
-      )}
+      <ImageSlider config={sliderConfig} />
     </div>
   )
 }
---
+++ src/components/ImageSlider/ImageSlider.js
@@ -0,0 +1,85 @@
+/* eslint-env browser */
+
+import React, { useRef, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { range } from 'ramda'
+
+import _ from './ImageSlider.module.sass'
+
+/**
+ * `images` is an array of entries looking like this:
+ * ```
+ * { src: 'https://url-to-your/image.jpg', label: 'Short Description' }
+ * ```
+ *
+ * @param {Object} props
+ */
+export default function ImageSlider ({ config }) {
+  const containerRef = useRef(null)
+  const [currentValue, setCurrentValue] = useState(config.min)
+  const [containerHeight, setContainerHeight] = useState('auto')
+
+  const adjustContainer = img => {
+    if (containerHeight === 'auto' || img.height > parseInt(containerHeight.replace(/px$/, ''), 10)) {
+      setContainerHeight(img.height + 'px')
+    }
+  }
+
+  // TODO: This is really wasteful, there should be a better way
+  const images = range(config.min, config.max + 1)
+    .filter(val => (val - config.min) % config.step === 0)
+    .map(val => [val, config.urlForValue(val)])
+
+  console.log('config', config)
+  console.log('images', images)
+
+  // when we load an image, the container should be as high as the tallest one
+  const updateContainerSize = e => adjustContainer(e.target)
+
+  // when the image size changes we update the container size accordingly
+  const monitorAvailableSpace = _ => {
+    if (containerRef.current != null) {
+      // FIXME: This is buggy and doesn't always work
+      setContainerHeight('auto')
+      window.requestAnimationFrame(_ => {
+        let imgs = Array.from(containerRef.current.querySelectorAll('img'))
+        imgs.forEach(img => adjustContainer(img))
+      })
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', monitorAvailableSpace)
+    return () => window.removeEventListener('resize', monitorAvailableSpace)
+  }, [])
+
+  return <div className={_.imageSliderComponent}>
+    <figure ref={containerRef} style={{ height: containerHeight }} className={_.images}>
+      {images.map(
+        ([val, src]) =>
+          <img
+            key={`image-slider-${val}`}
+            onLoad={updateContainerSize}
+            src={src}
+            className={currentValue === val ? _.selected : ''} />
+      )}
+    </figure>
+    <span className={_.imageSliderLabel}>{config.label} {currentValue}</span>
+    <div className={_.rangeContainer}>
+      <span className={_.labelMin}>{config.min}</span>
+      <input
+        type='range'
+        min={config.min}
+        max={config.max}
+        step={config.step}
+        defaultValue={currentValue}
+        onChange={e => setCurrentValue(parseInt(e.target.value), 10)} />
+      <span className={_.labelMax}>{config.max}</span>
+    </div>
+  </div>
+}
+
+// ImageSlider.propTypes = {
+//   images: PropTypes.array.isRequired,
+//   selectedImage: PropTypes.number
+// }
