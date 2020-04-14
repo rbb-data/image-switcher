169e03e8f40caeacd4875740268640ca1573a176
HalukaMB <mail@maier-borst.de>
2020-04-14 18:21:51 +0200

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,12 +9,18 @@ import ImageSlider from 'components/ImageSlider/ImageSlider'
 
 function App (_props) {
   const sliderConfig = {
-    urlForValue: val => `./images/effect/${val}.svg`,
-    labelForValue: val => `SIR-Kurve mit einem Maßnahmeneffekt von ${val}%`,
-    min: 10,
-    max: 90,
+    //urlForValue: val => `./images/duration_of_intervention/${val}Mod.svg`,
+    //labelForValue: val => `SIR-Kurve mit Maßnahmendauer von ${val}`,
+    //min: 4,
+    //max: 8,
+    //step: 1,
+    //label: 'Wochen mit aktuellen Einschränkungen:'
+    urlForValue: val => `./images/share_of_r0_after_intervention/${100-val}Mod.svg`,
+    labelForValue: val => `SIR-Kurve mit Effekitivität der Maßnahmen von ${100-val}%`,
+    min: 0,
+    max: 50,
     step: 10,
-    label: 'Effekt der Einschränkungen:'
+    label: 'Rückgang der Neuansteckungen pro Infizierten (in %):'
   }
 
   return (
