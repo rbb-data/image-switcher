96124668c9ef3c157e26251b6135d1d72fcd90f2
heyarne <arne@schlueter.is>
2020-09-18 17:38:38 +0200

    Pull recent data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,12 +9,13 @@ import ImageSlider from 'components/ImageSlider/ImageSlider'
 
 function App (_props) {
   const sliderConfig = {
-    urlForValue: val => `./images/corona-second-wave/mosaic_1_of_2_${val * 7}_days.png`,
-    labelForValue: val => `Trenddauer von ${val} Wochen`,
+    urlForValue: val => `./images/corona-second-wave/mosaic_2_of_2_${val * 7}_days.png`,
+    labelForValue: val => `Trenddauer von mindestens ${val} Woche(n)`,
     min: 1,
     max: 4,
     step: 1,
-    label: val => `Trenddauer von ${val} Wochen`
+    title: 'Wie sich die Epidemie in Bundesländern mit niedriger Inzidenz entwickelte',
+    label: val => `Trenddauer von mindestens ${val} Woche(n)`
   }
 
   return (
